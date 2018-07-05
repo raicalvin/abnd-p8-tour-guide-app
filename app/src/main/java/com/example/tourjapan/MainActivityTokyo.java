fd62e9151ff4d50604590db655d57f8aa895439a
Calvin Rai <raicalvin@gmail.com>
2018-07-05 16:57:18 -0700

    Added empty Tokyo Main Activity to project.

---
+++ app/src/main/java/com/example/tourjapan/MainActivityTokyo.java
@@ -0,0 +1,13 @@
+package com.example.tourjapan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityTokyo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tokyo);
+    }
+}
