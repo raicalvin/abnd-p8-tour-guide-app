79a146c4b1e1956fc6d41d62150a64fda9a73361
Calvin Rai <raicalvin@gmail.com>
2018-07-05 17:01:23 -0700

    Added references to RelativeLayouts in MainActivity for Main Menu items.

--- app/src/main/java/com/example/tourjapan/MainActivityTokyo.java
+++ app/src/main/java/com/example/tourjapan/MainActivityTokyo.java
@@ -2,6 +2,7 @@ package com.example.tourjapan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivityTokyo extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivityTokyo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tokyo);
+
+        RelativeLayout mainMenuNagoya = (RelativeLayout) findViewById(R.id.main_menu_nagoya);
+        RelativeLayout mainMenuNakatsugawa = (RelativeLayout) findViewById(R.id.main_menu_nakatsugawa);
+        RelativeLayout mainMenuTokyo = (RelativeLayout) findViewById(R.id.main_menu_tokyo);
+        RelativeLayout mainMenuKyoto = (RelativeLayout) findViewById(R.id.main_menu_kyoto);
+        RelativeLayout mainMenuOsaka = (RelativeLayout) findViewById(R.id.main_menu_osaka);
+
+
     }
 }
