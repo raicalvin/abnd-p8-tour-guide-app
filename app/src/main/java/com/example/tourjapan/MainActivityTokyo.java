1fce18153995610391508b07ed69e86983584168
Calvin Rai <raicalvin@gmail.com>
2018-07-08 17:29:49 -0700

    Implemented initial tab layout in Tokyo activity.

--- app/src/main/java/com/example/tourjapan/MainActivityTokyo.java
+++ app/src/main/java/com/example/tourjapan/MainActivityTokyo.java
@@ -1,11 +1,9 @@
 package com.example.tourjapan;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.design.widget.TabLayout;
 
 public class MainActivityTokyo extends AppCompatActivity {
 
@@ -13,5 +11,21 @@ public class MainActivityTokyo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tokyo);
+
+        // Set up the ViewPager to control lists for the tab items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager_tokyo);
+
+        // This adapter will know which fragments need to be shown on each page
+        PagerAdapterTokyo adapter = new PagerAdapterTokyo(this, getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs for Tokyo
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs_tokyo);
+
+        // Connect tab layout with the view pager
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
--- app/src/main/java/com/example/tourjapan/PagerAdapterTokyo.java
+++ app/src/main/java/com/example/tourjapan/PagerAdapterTokyo.java
@@ -1,13 +1,19 @@
 package com.example.tourjapan;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class PagerAdapterTokyo extends FragmentPagerAdapter {
 
-    public PagerAdapterTokyo(FragmentManager fm) {
+    /** Context of the app */
+    private Context mContext;
+
+    public PagerAdapterTokyo(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -31,4 +37,29 @@ public class PagerAdapterTokyo extends FragmentPagerAdapter {
         }
     }
 
+    /**
+     * This method may be called by the ViewPager to obtain a title string
+     * to describe the specified page. This method may return null
+     * indicating no title for this page. The default implementation returns
+     * null.
+     *
+     * @param position The position of the title requested
+     * @return A title for the requested page
+     */
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.tab_history);
+            case 1:
+                return mContext.getString(R.string.tab_places);
+            case 2:
+                return mContext.getString(R.string.tab_food);
+            case 3:
+                return mContext.getString(R.string.tab_secrets);
+            default:
+                return null;
+        }
+    }
 }
