9571af909b3fe367c9afc64b28b2e6f3054e008c
Calvin Rai <raicalvin@gmail.com>
2018-07-08 18:57:18 -0700

    Added History class to store history objects.

---
+++ app/src/main/java/com/example/tourjapan/History.java
@@ -0,0 +1,21 @@
+package com.example.tourjapan;
+
+public class History {
+
+    /**
+     * Private member variables to hold history event
+     * title and the date of the event
+     */
+    private String mEventTitle, mEventDate;
+
+    /**
+     * Create a new History object storing the event name, event date, and icon resource id
+     * @param eventTitle
+     * @param eventDate
+     */
+    public History(String eventTitle, String eventDate) {
+        mEventTitle = eventTitle;
+        mEventDate = eventDate;
+    }
+
+}
