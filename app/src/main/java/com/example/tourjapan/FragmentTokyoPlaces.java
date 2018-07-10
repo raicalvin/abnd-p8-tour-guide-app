c0d6083183c80a714deacc5ba6a711f767d5041c
Calvin Rai <raicalvin@gmail.com>
2018-07-09 17:32:05 -0700

    Updated PlacesAdapter.

--- app/src/main/java/com/example/tourjapan/FragmentTokyoPlaces.java
+++ app/src/main/java/com/example/tourjapan/FragmentTokyoPlaces.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +25,39 @@ public class FragmentTokyoPlaces extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+
+        View rootView = inflater.inflate(R.layout.list_view, container, false);
+
+        final ArrayList<Places> places = new ArrayList<>();
+
+        places.add(new Places("The great something of japan", R.drawable.filler1));
+        places.add(new Places("The great something of asia", R.drawable.filler2));
+        places.add(new Places("The great something of canada", R.drawable.filler3));
+        places.add(new Places("The great something of singapore", R.drawable.filler1));
+        places.add(new Places("The great something of iceland", R.drawable.filler2));
+        places.add(new Places("The great something of kyoto", R.drawable.filler2));
+        places.add(new Places("The great something of osaka", R.drawable.filler3));
+        places.add(new Places("The great something of nagoya", R.drawable.filler1));
+        places.add(new Places("The great something of nara", R.drawable.filler3));
+        places.add(new Places("The great something of mars", R.drawable.filler1));
+        places.add(new Places("The great something of venus", R.drawable.filler2));
+        places.add(new Places("The great something of jupiter", R.drawable.filler1));
+        places.add(new Places("The great something of target", R.drawable.filler2));
+        places.add(new Places("The great something of panera bread", R.drawable.filler1));
+        places.add(new Places("The great something of gomez", R.drawable.filler1));
+        places.add(new Places("The great something of magome", R.drawable.filler1));
+        places.add(new Places("The great something of naha", R.drawable.filler2));
+        places.add(new Places("The great something of chiba", R.drawable.filler3));
+        places.add(new Places("The great something of kobe", R.drawable.filler1));
+
+        PlacesAdapter adapter = new PlacesAdapter(getActivity(), places);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
+
     }
 
 }
---
+++ app/src/main/java/com/example/tourjapan/PlacesAdapter.java
@@ -0,0 +1,40 @@
+package com.example.tourjapan;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class PlacesAdapter extends ArrayAdapter {
+
+    public PlacesAdapter(Context context, ArrayList<Places> places) {
+        super(context, 0, places);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Get the Places item to use
+        Places placeItem = (Places) getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView placeTitleTV = (TextView) convertView.findViewById(R.id.history_title_text_view);
+        ImageView placeImageIV = (ImageView) convertView.findViewById(R.id.image_icon);
+
+        placeTitleTV.setText(placeItem.getPlaceTitle());
+        placeImageIV.setImageResource(placeItem.getPicResourceID());
+
+        return convertView;
+    }
+}
