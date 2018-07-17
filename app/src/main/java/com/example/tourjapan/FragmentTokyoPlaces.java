4b4f798ceb0708f49c3872ecf63b157b88aa383b
Calvin Rai <raicalvin@gmail.com>
2018-07-17 10:58:50 -0700

    Updated Places fragment to store header image resource ID for all places.

--- app/src/main/java/com/example/tourjapan/FragmentTokyoPlaces.java
+++ app/src/main/java/com/example/tourjapan/FragmentTokyoPlaces.java
@@ -32,16 +32,16 @@ public class FragmentTokyoPlaces extends Fragment {
 
         final ArrayList<Places> places = new ArrayList<>();
 
-        places.add(new Places("Akihabara Station", R.drawable.places_icon_akihabara_station, getString(R.string.place_1)));
-        places.add(new Places("Ameyoko Shopping Street", R.drawable.places_icon_ameyoko_shopping_street, getString(R.string.place_2)));
-        places.add(new Places("Ginza", R.drawable.places_icon_ginza, getString(R.string.place_3)));
-        places.add(new Places("Imperial Palace", R.drawable.places_icon_imperial_palace, getString(R.string.place_1)));
-        places.add(new Places("Meiji Temple", R.drawable.places_icon_meiji_temple, getString(R.string.place_2)));
-        places.add(new Places("Nissan Crossing", R.drawable.places_icon_nissan_crossing, getString(R.string.place_2)));
-        places.add(new Places("Senso-Ji Temple", R.drawable.places_icon_sensoji_temple, getString(R.string.place_3)));
-        places.add(new Places("Shinjuku", R.drawable.places_icon_shinjuku, getString(R.string.place_3)));
-        places.add(new Places("Tokyo Skytree", R.drawable.places_icon_tokyo_skytree, getString(R.string.place_3)));
-        places.add(new Places("Ueno Park", R.drawable.places_icon_ueno_park, getString(R.string.place_1)));
+        places.add(new Places("Akihabara Station", R.drawable.places_icon_akihabara_station, getString(R.string.place_1), R.drawable.places_akihabara_station));
+        places.add(new Places("Ameyoko Shopping Street", R.drawable.places_icon_ameyoko_shopping_street, getString(R.string.place_2), R.drawable.places_ameyoko_shopping_street));
+        places.add(new Places("Ginza", R.drawable.places_icon_ginza, getString(R.string.place_3), R.drawable.places_ginza));
+        places.add(new Places("Imperial Palace", R.drawable.places_icon_imperial_palace, getString(R.string.place_1), R.drawable.places_imperial_palace));
+        places.add(new Places("Meiji Temple", R.drawable.places_icon_meiji_temple, getString(R.string.place_2), R.drawable.places_meiji_temple));
+        places.add(new Places("Nissan Crossing", R.drawable.places_icon_nissan_crossing, getString(R.string.place_2), R.drawable.places_nissan_crossing));
+        places.add(new Places("Senso-Ji Temple", R.drawable.places_icon_sensoji_temple, getString(R.string.place_3), R.drawable.places_sensoji_temple));
+        places.add(new Places("Shinjuku", R.drawable.places_icon_shinjuku, getString(R.string.place_3), R.drawable.places_shinjuku));
+        places.add(new Places("Tokyo Skytree", R.drawable.places_icon_tokyo_skytree, getString(R.string.place_3), R.drawable.places_tokyo_skytree));
+        places.add(new Places("Ueno Park", R.drawable.places_icon_ueno_park, getString(R.string.place_1), R.drawable.places_ueno_park));
 
         PlacesAdapter adapter = new PlacesAdapter(getActivity(), places);
 
@@ -54,7 +54,7 @@ public class FragmentTokyoPlaces extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(rootView.getContext(), PlacesActivity.class);
                 intent.putExtra("Place-Description", places.get(position).getPlaceDescription());
-                intent.putExtra("Place-Image", places.get(position).getPicResourceID());
+                intent.putExtra("Place-Image", places.get(position).getHeaderResourceID());
                 startActivity(intent);
             }
         });
